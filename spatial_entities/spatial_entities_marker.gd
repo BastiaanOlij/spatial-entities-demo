723478d034e91c7c3584ecd4b040a0726359d59a
Bastiaan Olij <mux213@gmail.com>
2025-09-22 18:16:26 +1000

    Tweaked marker material and added a plushy QR Code

--- spatial_entities/spatial_entities_marker.gd
+++ spatial_entities/spatial_entities_marker.gd
@@ -10,13 +10,30 @@ func _ready():
 	if marker_tracker:
 		match marker_tracker.marker_type:
 			OpenXRSpatialComponentMarkerList.MARKER_TYPE_QRCODE:
-				text = "QR code marker"
-
 				var data : Variant = marker_tracker.get_marker_data()
+				var marker_file : String = ""
 				if typeof(data) == TYPE_STRING:
-					text += "\nData: " + data
-				elif typeof(data) == TYPE_PACKED_BYTE_ARRAY:
-					text += "\nData: " + data.hex_encode()
+					var data_str : String = data
+					marker_file = "res://markers/scenes/" + data_str.to_lower() + ".tscn";
+					# "res://markers/scenes/plush.tscn"
+
+				if ResourceLoader.exists(marker_file):
+					text = ""
+					var scene : PackedScene = load(marker_file);
+					if scene:
+						var instance = scene.instantiate()
+						if instance:
+							add_child(instance)
+						else:
+							text = "Failed to instantiate " + data
+					else:
+						text = "Failed to load " + data
+				else:
+					text = "QR code marker"
+					if typeof(data) == TYPE_STRING:
+						text += "\nData: " + marker_file
+					elif typeof(data) == TYPE_PACKED_BYTE_ARRAY:
+						text += "\nData: " + data.hex_encode()
 			OpenXRSpatialComponentMarkerList.MARKER_TYPE_MICRO_QRCODE:
 				text = "Micro QR code marker"
 
